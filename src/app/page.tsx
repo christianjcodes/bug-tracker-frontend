8ac3fa078995ee2943af8c50931c651ad088e962
Christian Johnson <cartian4@gmail.com>
2023-10-27 06:17:08 -0400

    Add email and password inputs to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,16 @@ export default function Home() {
   return (
     <Layout>
       <div className='loginArea'>
-
+        <Form.Label htmlFor="inputEmail">Email</Form.Label>
+        <Form.Control
+          type="username"
+          id="inputEmail"
+        />
+        <Form.Label htmlFor="inputPassword">Password</Form.Label>
+        <Form.Control
+          type="password"
+          id="inputPassword"
+        />
       </div>
     </Layout>
   );
