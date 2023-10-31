9bf66c8ad24fa1f52f62a23f9cbda4d172f3c107
Christian Johnson <cartian4@gmail.com>
2023-10-31 06:28:34 -0400

    new-user page changes

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,7 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Layout from '../layout';
+
 export default function Page() {
     return (
         <div>
-            <h1>new user page</h1>            
+            <h1>
+                New Yooser Page
+            </h1>
         </div>
+        // <Layout>
+        //     <Form>
+        //         <Row className='mb-3'>
+        //             <Form.Group as={Col} controlId='formGridEmail'>
+        //                 <Form.Label>Email</Form.Label>
+        //                 <Form.Control type="email" placeholder='Enter email'/>
+        //             </Form.Group>
+        //         </Row>
+        //     </Form>
+        // </Layout>
     );
 }
