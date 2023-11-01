0fd40fc44ef2a811ee346926b6b4188b0a2d8b4a
Christian Johnson <cartian4@gmail.com>
2023-11-01 06:36:14 -0400

    Remove bootstrap elements from new-user page, switch to mui-material

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,25 +1,71 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Layout from '../layout';
+'use client'
+import * as React from "react";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff'; 
+import type { NextPage } from 'next';
 
-export default function Page() {
+
+const Page: NextPage = () => {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+  
     return (
-        <div>
-            <h1>
-                New Yooser Page
-            </h1>
-        </div>
-        // <Layout>
-        //     <Form>
-        //         <Row className='mb-3'>
-        //             <Form.Group as={Col} controlId='formGridEmail'>
-        //                 <Form.Label>Email</Form.Label>
-        //                 <Form.Control type="email" placeholder='Enter email'/>
-        //             </Form.Group>
-        //         </Row>
-        //     </Form>
-        // </Layout>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div>
+                <TextField
+                    label="First Name"
+                    id="outlined-start-adornment"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+                <TextField
+                    label="Last Name"
+                    id="outlined-start-adornment"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+                <TextField
+                    label="Email Address"
+                    id="outlined-start-adornment"
+                    sx={{ m: 1, width: '25ch' }}
+                />
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
+                    />
+                </FormControl>
+            </div>
+        </Box>    
     );
-}
+};
+
+export default Page;
