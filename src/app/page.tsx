dbf3e9123db8cb02ae232609c912248478f602dd
Christian Johnson <cartian4@gmail.com>
2023-11-03 06:17:05 -0400

    Add mui material to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,64 @@
-import Image from 'next/image';
+'use client'
 import styles from './page.module.css';
-import Layout from './layout';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import Link from 'next/link';
+import * as React from "react";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className='loginArea'>
-        <Form.Label htmlFor="inputEmail">Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder='Enter email'
-          id="inputEmail"
-        />
-        <Form.Label htmlFor="inputPassword">Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder='Password'
-          id="inputPassword"
-        />
-        <Link href="/forgot-password">Forgot password?</Link>
-      </div>
-      <Button variant="primary">Sign In</Button>{' '}
-      <Link href="/new-user">New user? Create an account here!</Link>
 
-    </Layout>
+  const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
+
+  return (
+    <div className='login'>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div>
+          <TextField
+              label="Email Address"
+              id="email-entry"
+              sx={{ m: 1, width: '25ch' }}
+          />
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+              <InputLabel htmlFor="password-entry">Password</InputLabel>
+              <OutlinedInput
+              id="password-entry"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                  <InputAdornment position="end">
+                  <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                  >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                  </InputAdornment>
+              }
+              label="Password"
+              />
+          </FormControl>
+        </div>
+      </Box>  
+      
+      <div className='links'>
+        <Link href="/forgot-password">Forgot password?</Link>
+        <Link href="/new-user">New user? Create an account here!</Link>
+      </div>
+    </div>
   );
 }
