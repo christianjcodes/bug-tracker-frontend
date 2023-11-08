9ea46014088e36f793dc2c1e40815e5897920b2f
Christian Johnson <cartian4@gmail.com>
2023-11-08 06:23:19 -0500

    import fontawesome and add sidebaricon bug

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,14 +1,18 @@
+import { FaBug } from "react-icons/fa";
+
 const SideBar = () => {
 
     return (
         <div className="fixed top-0 left-0 h-screen w-64 m-0
                         flex flex-col bg-teal-500 text-black
                         shadow-lg">
-            <i>A</i>
+            <SideBarIcon icon={<FaBug size="28" />} />
+            <i>Projects</i>
             <i>B</i>
             <i>C</i>
             <i>D</i>
             <i>E</i>
+
         </div>
     )
 };
