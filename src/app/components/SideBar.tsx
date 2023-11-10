a5ef94d25970cd19b1248db7107d3f57b3ce08cc
Christian Johnson <cartian4@gmail.com>
2023-11-10 06:06:06 -0500

    Add link to sign in page in sign out button

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -7,22 +7,21 @@ const SideBar = () => {
 
     return (
         <div className="fixed top-0 left-0 h-screen w-44 m-0
-                        flex flex-col bg-stone-50 text-black
+                        flex flex-col bg-stone-50 text-slate-600
                         shadow-lg">
-            <div className="flex flex-row">
+            <div className="flex flex-row text-teal-500">
                 <SideBarIcon icon={<FaBug size="38" />} />
                 <h1 className="ml-2 mt-4 font-bold">Bug Tracker</h1>
             </div>
             <div className="ml-2 space-y-4">
-                <h3 className="hover:text-teal-500"><Link href="/dashboard">Dashboard</Link></h3>
-                <h3 className="hover:text-teal-500"><Link href="/tickets">Tickets</Link></h3>
-                <h3 className="hover:text-teal-500"><Link href="/admin">Administration</Link></h3>
+                <h3 className="hover:text-cyan-500"><Link href="/dashboard">Dashboard</Link></h3>
+                <h3 className="hover:text-cyan-500"><Link href="/tickets">Tickets</Link></h3>
+                <h3 className="hover:text-cyan-500"><Link href="/admin">Administration</Link></h3>
             </div>
             <div className="flex justify-center items-center mt-10">
-                <Button variant="outlined" size="medium">Sign Out</Button>
-            </div>
-            <div>
-                <SideBarIcon icon={<FaGithub size="28" />} />
+                <Link href="/">
+                    <Button variant="outlined" size="medium">Sign Out</Button>
+                </Link>
             </div>
         </div>
     )
