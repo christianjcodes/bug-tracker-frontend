98a4522554f2340fb42f9879d4262b407ba369a0
Christian Johnson <cartian4@gmail.com>
2023-11-13 05:44:59 -0500

    Add Footer to global layout, change size of github icon

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
@@ -5,11 +6,10 @@ export default function Footer() {
     return (
         <footer>
             <div>
-                <p>Christian Johnson &copy; {new Date().getFullYear()} 
-                    <FaGithub>
-                        <Link href="https://github.com/christianjcodes"></Link>
-                    </FaGithub>
-                </p>
+                <p>Christian Johnson &copy; {new Date().getFullYear()}</p>
+                <Link href="https://github.com/christianjcodes">
+                    <FaGithub size="28"/>
+                </Link>
             </div>
         </footer>
     );
