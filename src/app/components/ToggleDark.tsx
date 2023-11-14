ff1482c103741f68ca76c2c560cf49f8f30c0c7c
Christian Johnson <cartian4@gmail.com>
2023-11-14 06:10:20 -0500

    Add useEffect function and modify toggleTheme

--- src/app/components/ToggleDark.tsx
+++ src/app/components/ToggleDark.tsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as React from "react";
 import { FaRegSun, FaRegMoon } from "react-icons/fa";
 
-const [theme] = useState('');
+const [theme, setTheme] = useState('');
+
+useEffect(() => {
+
+    const theme = JSON.parse(localStorage.getItem('theme') || '{}');
+
+    if ((!theme && 
+      window.matchMedia('(prefers-color-scheme: dark)').matches) || theme === 'dark') {
+      setTheme('dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      setTheme('light');
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
 
 function toggleTheme() {
-    if (theme === 'light')
+    if (theme === 'light') {
+        localStorage.setItem('theme', JSON.stringify('dark'));
+        setTheme('dark');
+        document.documentElement.classList.add('dark');
+    } else {
+        localStorage.setItem('theme', JSON.stringify('light'));
+        setTheme('light');
+        document.documentElement.classList.remove('dark');
+    }
+
 }
 
 export default function ToggleDark() {
