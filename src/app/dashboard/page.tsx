b0e0f4f4289b09fe4c1523092db35ee865d051ba
Christian Johnson <cartian4@gmail.com>
2023-11-27 06:29:06 -0500

    Troubleshoot dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,19 +4,21 @@ import Waves from '../components/Waves';
 
 export default function Page() {
     return (
-        <div className='h-screen w-screen flex flex-col align-bottom
+        <div className='h-screen w-screen flex flex-col
             bg-gradient-to-br from-slate-50 to-emerald-300 overflow-hidden'>
             
-            <div className='flex h-[70vh]'>
+            {/* <div className='flex h-[70vh]'>
                 <SideBar />
-            </div>
+            </div> */}
+            <div className='flex items-center justify-center'>
+                <div className='w-full max-w-xl'>
+                    <div className='bg-white shadow-md rounded px-8'>
 
-            <div className='w-full max-w-xl'>
-                <div className='bg-white shadow-md rounded px-8 justify-center space-x-1.5'>
-
+                    </div>
                 </div>
             </div>
-            <Waves />
+            
+            {/* <Waves /> */}
         </div>
     );
 }
