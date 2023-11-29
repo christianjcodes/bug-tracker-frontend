1b984150f9a755ce74b852a988f077808566df89
Christian Johnson <cartian4@gmail.com>
2023-11-29 06:28:28 -0500

    Fix container issue in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react';
 import SideBar from '../components/SideBar';
-import Waves from '../components/Waves';
 
 export default function Page() {
     return (
-        <div className='h-screen w-screen flex flex-col
-            bg-gradient-to-br from-slate-50 to-emerald-300 overflow-hidden'>
-            
-            {/* <div className='flex h-[70vh]'>
+        <>
+            <div className='h-screen w-screen flex flex-col
+                bg-gradient-to-br from-slate-50 to-emerald-300 overflow-hidden'>    
+                
                 <SideBar />
-            </div> */}
-            <div className='flex items-center justify-center'>
-                <div className='w-full max-w-xl'>
-                    <div className='bg-white shadow-md rounded px-8'>
-
-                    </div>
-                </div>
-            </div>
             
-            {/* <Waves /> */}
-        </div>
+                <main className='flex items-center justify-center flex-col h-[75vh]'>
+                    <h1>Main Content Goes Here</h1>
+                </main>
+                
+            </div>
+        </>
     );
 }
