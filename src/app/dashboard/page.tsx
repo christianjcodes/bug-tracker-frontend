b621f3d4ac5f70a9f249f23a49bf8d404f149f3f
Christian Johnson <cartian4@gmail.com>
2023-12-06 06:15:21 -0500

    Add new project button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,10 +12,24 @@ export default function Page() {
                 <h1 className='mt-4 ml-[17rem] mb-4 text-3xl 
                     text-sky-500 drop-shadow-md'>Dashboard</h1>
             
-                <div id='projects' className='flex flex-col 
+                <div id='projects' className='flex flex-row 
                     h-[45vh] mb-[5vh] ml-[17rem] mr-[10vh] bg-white
                     shadow-lg rounded-lg'>
                     <h1>Projects Go Here</h1>
+                    <button className='
+                        bg-sky-500 
+                        hover:bg-sky-600 
+                        text-white 
+                        font-semibold 
+                        h-10
+                        w-21
+                        py-2 
+                        px-4 
+                        rounded 
+                        focus:outline-none 
+                        focus:shadow-outline" type="button'>
+                        New Project
+                    </button>
                 </div>
 
                 <div id='stats' className='flex flex-col 
