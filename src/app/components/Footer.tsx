6801c32028d73bb0b08d12ce30a00ad7de6c65ce
Christian Johnson <cartian4@gmail.com>
2023-12-15 06:30:24 -0500

    Change background and footer link hover

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <footer className='absolute bottom-0 flex flex-row justify-center w-full text-slate-600
                                 space-x-4 pb-4 dark:text-white ml-auto mr-auto'>
                 <p>Christian Johnson &copy; {new Date().getFullYear()}</p>
-                <div className='hover:text-slate-950'>
+                <div className='hover:text-white'>
                     <Link href="https://github.com/christianjcodes">
                         <FaGithub size="24"/>
                     </Link>
