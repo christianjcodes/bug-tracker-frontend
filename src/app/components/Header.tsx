109218a1a2adfd58dd8c23f56fd482fda3c7bb88
Christian Johnson <cartian4@gmail.com>
2023-12-18 05:46:16 -0500

    Add Header to components

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='fixed w-full h-24 shadow-xl bg-white'>
+        Header
+    </nav>
+  );
+}
+
+export default Header
