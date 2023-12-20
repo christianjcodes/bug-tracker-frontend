435d267248a00b3a430aac471195bcabff0a9079
Christian Johnson <cartian4@gmail.com>
2023-12-20 05:45:27 -0500

    Add local background to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import LoginForm from "./components/LoginForm";
+import styles from "./page.module.css";
 
 
 export default function Home() {
 
   return (
-    <div className='h-screen w-screen flex flex-col align-bottom'>
-      <div className='h-[70vh] flex justify-center items-center'>
-        <LoginForm />
+    <div className={styles.homePage}>
+      <div className='h-screen w-screen flex flex-col align-bottom'>
+        <div className='h-[70vh] flex justify-center items-center'>
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
