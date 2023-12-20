53edb1c4dcf07994555fc5cd53a799d38a420fd0
Christian Johnson <cartian4@gmail.com>
2023-12-20 06:43:59 -0500

    Add NextThemeProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+'use client'
 import * as React from "react";
 import LoginForm from "./components/LoginForm";
 import styles from "./page.module.css";
+import { useTheme } from "next-themes";
 
 
 export default function Home() {
 
+  const { systemTheme, theme } = useTheme();
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  var classStyle;
+
+  if (currentTheme == 'dark') {
+    classStyle = styles.homePageDark;
+  }  
+
   return (
-    <div className={styles.homePage}>
+    <div className={styles.homePageDark}>
       <div className='h-screen w-screen flex flex-col align-bottom'>
         <div className='h-[70vh] flex justify-center items-center'>
           <LoginForm />
