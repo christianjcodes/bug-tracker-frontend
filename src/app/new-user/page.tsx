0b78187e82e612dc4dc9e7c7e2a54b532602c3a2
Christian Johnson <cartian4@gmail.com>
2023-12-21 06:28:15 -0500

    Add background to new user page, credit svgbackgrounds in page.module.css

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import SignUpForm from "../components/SignUpForm";
+import styles from "../page.module.css";
 
 
 export default function Page() {
     return (
-      <div className='h-screen w-screen flex flex-col align-bottom'>
-        <div className='h-[70vh] flex justify-center items-center'>
-          <SignUpForm />
+      <div className={styles.homePageDark}>
+        <div className='h-screen w-screen flex flex-col align-bottom'>
+          <div className='h-[70vh] flex justify-center items-center'>
+            <SignUpForm />
+          </div>
         </div>
       </div>
     );
