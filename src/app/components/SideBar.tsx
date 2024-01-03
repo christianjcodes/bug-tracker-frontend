3a07d686e3629919c57b8be5c723b182b57bc620
Christian Johnson <cartian4@gmail.com>
2024-01-03 06:02:45 -0500

    Add lib folder and utils.ts

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -5,7 +5,8 @@ type Props = {}
 export default function SideBar({}: Props) {
   return (
     <nav className='min-h-screen max-h-screen mt-[4rem] overflow-y-auto w-fit md:pr-8
-        pr-3 pt-2 flex flex-col gap-3 border-r-[1px] dark:border-r-slate-700 pl-[50px] dark:bg-slate-800'>
+        pr-3 pt-2 flex flex-col gap-3 border-r-[1px] 
+        dark:border-r-slate-700 pl-[50px] dark:bg-slate-800 dark:text-white'>
         SideBar
     </nav>
   )
---
+++ src/app/lib/utils.ts
@@ -0,0 +1,6 @@
+
+
+export function cn() {
+    
+    return
+}
