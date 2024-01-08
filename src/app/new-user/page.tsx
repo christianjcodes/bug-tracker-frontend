684e73a82aa94bc1aa167d6c1d8c376eac7b2260
Christian Johnson <cartian4@gmail.com>
2024-01-08 06:20:07 -0500

    Changed style name for homepage and new user page

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -5,7 +5,7 @@ import styles from "../page.module.css";
 
 export default function Page() {
     return (
-      <div className={styles.homePageDark}>
+      <div className={styles.homePage}>
         <div className='h-screen w-screen flex flex-col align-bottom'>
           <div className='h-[70vh] flex justify-center items-center'>
             <SignUpForm />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,12 @@
 import * as React from "react";
 import LoginForm from "./components/LoginForm";
 import styles from "./page.module.css";
-import { useTheme } from "next-themes";
 
 
-export default function Home() {
-
-  // System theme code currently not functioning, will replace later
-
-  const { systemTheme, theme } = useTheme();
-
-  const currentTheme = theme === 'system' ? systemTheme : theme;
-
-  let classStyle;
-
-  if (currentTheme == 'dark') {
-    classStyle = styles.homePageDark;
-  }  
+export default function Home() { 
 
   return (
-    <div className={styles.homePageDark}>
+    <div className={styles.homePage}>
       <div className='h-screen w-screen flex flex-col align-bottom'>
         <div className='h-[70vh] flex justify-center items-center'>
           <LoginForm />
